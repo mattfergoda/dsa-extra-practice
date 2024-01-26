af558bcd6547ff1f13ad0706139ad452319fc893
Matt Fergoda <mattfergoda@users.noreply.github.com>
2024-01-26 13:18:59 -0800

    Works recursively.

---
+++ balancedBrackets.js
@@ -0,0 +1,60 @@
+const OPEN_BRACKETS = ["{", "[", "("];
+const CLOSED_BRACKETS = [")", "]", "}"];
+
+function balancedBrackets(input) {
+
+  function _findMatch(input, index, brackets) {
+
+    if (index === input.length) return brackets.length === 0;
+
+    const thisChar = input[index];
+
+    if (!OPEN_BRACKETS.includes(thisChar) && !CLOSED_BRACKETS.includes(thisChar)) {
+      return _findMatch(input, index + 1, brackets);
+    }
+
+    if (OPEN_BRACKETS.includes(thisChar)) {
+      brackets.push(thisChar);
+      return _findMatch(input, index + 1, brackets);
+    } 
+
+    const bracket = brackets[brackets.length - 1];
+
+    if (bracket === '{') {
+      if (thisChar === '}') {
+        brackets.pop();
+        return _findMatch(input, index + 1, brackets);
+
+      }
+      return _findMatch(input, index + 1, brackets);
+    }
+
+    if (bracket === '[') {
+      if (thisChar === ']') {
+        brackets.pop();
+        return _findMatch(input, index + 1, brackets);
+
+      }
+      return _findMatch(input, index + 1, brackets);
+    }
+
+    if (bracket === '(') {
+      if (thisChar === ')') {
+        brackets.pop();
+        return _findMatch(input, index + 1, brackets);
+
+      }
+      return _findMatch(input, index + 1, brackets);
+    }
+
+    if (CLOSED_BRACKETS.includes(thisChar)) {
+      return false;
+    } 
+  }
+
+  return _findMatch(input, 0, []);
+
+}
+
+balancedBrackets("(hi [there])");
+
--- josephus.test.js
+++ josephus.test.js
@@ -2,6 +2,9 @@ const { findSurvivor } = require("./josephus");
 
 describe("findSurvivor", function() {
   it("finds survivor", function() {
-    expect(findSurvivor(10, 3)).toEqual(4)
+    expect(findSurvivor(10, 3)).toBe(4);
+    expect(findSurvivor(5, 10)).toBe(4);
+    expect(findSurvivor(1, 1)).toBe(1);
+    expect(findSurvivor(2, 2)).toBe(1);
   });
 })
